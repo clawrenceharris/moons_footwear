257ae4eb0df5cc17ec961807e87e41be36d4f6d1
Caleb Harris <clawrenceharris@gmail.com>
2025-05-26 16:59:36 -0400

    basename changed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter as BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/moons_footwear">
+    <BrowserRouter>
       <NavProvider>
         <ShopProvider>
           <App />
